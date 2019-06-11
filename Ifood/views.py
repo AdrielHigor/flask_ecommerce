ba7db510db70dc0a1879cdebb1c943ede0ab2e5f
adrielhigor <adrielhigor@gmail.com>
2019-06-11 15:01:03 -0300

    file upload in progress work

--- Ifood/views.py
+++ Ifood/views.py
@@ -3,6 +3,8 @@ from flask import render_template, request, redirect, url_for
 from Ifood import app, db
 from Ifood.models import User, Product
 from Ifood.forms import LoginForm
+from werkzeug import secure_filename
+import os
 
 # ...
 
@@ -22,3 +24,16 @@ def home():
 def login():
     form = LoginForm()
     return render_template('Auth/index.html', form=form)
+
+@app.route('/upload')
+def upload_file():
+   return render_template('uploads/upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader():
+   if request.method == 'POST':
+      f = request.files['file']
+      filename = secure_filename(f.filename)
+      print(os.path.join(app.config['UPLOAD_FOLDER']))
+      #f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      return 'file uploaded successfully'
