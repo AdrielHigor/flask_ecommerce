6058f219a2181c09912a7e82fc7e92ffa994887a
Iarlyson <Iarlyson.Santana@outlook.com>
2019-06-10 22:32:29 -0300

    database att

--- Ifood/__init__.py
+++ Ifood/__init__.py
@@ -3,7 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+UPLOAD_FOLDER = '/Ifood/static/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
