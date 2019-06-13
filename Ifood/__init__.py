a88bcb411a89b0fbb6d84bff4fed25bafa013428
AdrielHigor <adrielhigor@gmail.com>
2019-06-13 11:40:03 -0300

    teste

--- Ifood/__init__.py
+++ Ifood/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+
+
+UPLOAD_FOLDER = 'Ifood/static/uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- Ifood/views.py
+++ Ifood/views.py
@@ -1,15 +1,43 @@
 from flask import render_template, request, redirect, url_for
 
-from Ifood import app, db
+from Ifood import app, db, ALLOWED_EXTENSIONS
 from Ifood.models import User, Product
 from Ifood.forms import LoginForm
+from werkzeug.utils import secure_filename
+import os
+...
 
-# ...
+# def allowed_file(filename):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @app.route('/login')
-# def login():
-#     form = LoginForm()
-#     return render_template('index.html', title='Sign In', form=form)
+# @app.route('/teste', methods=['GET', 'POST'])
+# def teste():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             flash('No selected file')
+#             return redirect(request.url)
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             return redirect(url_for('uploaded_file',
+#                                     filename=filename))
+#     return '''
+#     <!doctype html>
+#     <title>Upload new File</title>
+#     <h1>Upload new File</h1>
+#     <form method=post enctype=multipart/form-data>
+#       <p><input type=file name=file>
+#          <input type=submit value=Upload>
+#     </form>
+#     '''
 
 
 @app.route('/')
